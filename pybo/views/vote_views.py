5865eacf3a2a7990a243b1a5383532bff56fa8a4
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-11 01:00:40 +0900

    feat: 질문 추천 에러 메시지 수정

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -6,7 +6,7 @@ from ..models import Question, Answer
 def vote_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.user == question.author:
-        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
+        messages.error(request, '본인이 작성한 질문은 추천할 수 없습니다.')
     else:
         question.voter.add(request.user)
     return redirect('pybo:detail', question_id=question.id)
