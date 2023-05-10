7b2fe2385ba62eee98573a2a86085daeb7147532
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-11 00:57:45 +0900

    Revert "feat: 답변 추천 핸들러 추가" 이름 수정
    This reverts commit 859ba14dd9c62177051e98c9d2f7f0d57c66c478.

--- pybo/urls.py
+++ pybo/urls.py
@@ -35,5 +35,4 @@ urlpatterns = [
 
     # 추천
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
-    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
