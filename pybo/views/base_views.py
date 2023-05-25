7d533b2747c6570c223fab1d1b371f6c10731b3e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-26 00:41:28 +0900

    feat: 질문 리스트 정렬 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 from ..models import Question
 
 
@@ -10,7 +10,14 @@ def index(request):
     """
     page = request.GET.get("page", "1")  # page
     kw = request.GET.get('kw', '')
-    question_list = Question.objects.order_by('-create_date')
+    so = request.GET.get('so', 'recent')
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
+
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |
@@ -21,7 +28,7 @@ def index(request):
         ).distinct()
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     # return HttpResponse("안녕하세요! pybo에 오신 것을 환영합니다!")
     return render(request, 'pybo/question_list.html', context)
 
