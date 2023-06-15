e0da950af7e3fe15acf1dce535ffa5a8b35acba8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-06-15 10:31:37 +0900

    feat: owner 지정

--- post/views.py
+++ post/views.py
@@ -1,22 +1,25 @@
-from rest_framework.viewsets import ModelViewSet
-from .serializer import PostSerializer
-from .models import Post
 from rest_framework import generics
-
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
+from .models import Post
+from .serializer import PostSerializer
 
 # Create your views here.
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class PublicPostListAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class PublicPostListAPIView(APIView):
@@ -24,6 +27,8 @@ class PublicPostListAPIView(APIView):
         qs = Post.objects.filter(is_public=True)
         serializer = PostSerializer(qs, many=True)
         return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 public_post_list = PublicPostListAPIView.as_view()
